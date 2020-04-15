49b33d73d45c7be4f77cf15d30b79a4d5b3f7779
olfaa1 <49199466+olfaa1@users.noreply.github.com>
2020-04-15 22:55:57 +0100

    services scolarite

---
+++ src/Services/ServiceCoeff.java
@@ -0,0 +1,277 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Entities.Classe;
+import Entities.Matiere;
+import Entities.Coeff;
+import Utils.ConnexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.Alert;
+
+/**
+ *
+ * @author dell
+ */
+public class ServiceCoeff {
+
+    Connection c = ConnexionBD.getinstance().getcnx();
+
+    public List< Classe> ComboxGetLibClasse(String id) {
+        ArrayList< Classe> myList = new ArrayList();
+        String requete = "select libelle from `classe` where id = ? ";
+        try {
+            PreparedStatement pst = c.prepareStatement(requete);
+
+            pst.setString(1, id);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Classe a = new Classe();
+                a.setLibelle(rs.getString(1));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public List< Matiere> ComboxGetNomMatiere(String id) {
+        ArrayList< Matiere> myList = new ArrayList();
+        String requete = "select nom  from `matiere` where id = ? ";
+        try {
+            PreparedStatement pst = c.prepareStatement(requete);
+
+            pst.setString(1, id);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Matiere a = new Matiere();
+                // a.setId(rs.getInt(1));
+                a.setNom(rs.getString(1));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public List< Classe> ComboxGetIdClasse(String libelle) {
+        ArrayList< Classe> myList = new ArrayList();
+        String requete = "select id from `classe` where libelle = ? ";
+        try {
+            PreparedStatement pst = c.prepareStatement(requete);
+
+            pst.setString(1, libelle);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Classe a = new Classe();
+                a.setId(rs.getString(1));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public List< Matiere> ComboxGetIdMatiere(String nom) {
+        ArrayList< Matiere> myList = new ArrayList();
+        String requete = "select id , nom from `matiere` where nom = ? ";
+        try {
+            PreparedStatement pst = c.prepareStatement(requete);
+
+            pst.setString(1, nom);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Matiere a = new Matiere();
+                a.setId(rs.getString(1));
+                a.setNom(rs.getString(2));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public List< Classe> fillNiv() {
+
+        ArrayList< Classe> myList = new ArrayList();
+        try {
+            PreparedStatement pt = c.prepareStatement("select id,libelle from `classe` ");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                Classe a = new Classe();
+                a.setId(rs.getString(1));
+                a.setLibelle(rs.getString(2));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public List< Matiere> fillMatieres() {
+
+        ArrayList< Matiere> myList = new ArrayList();
+        try {
+            PreparedStatement pt = c.prepareStatement("select id,nom from `matiere` ");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                Matiere a = new Matiere();
+                a.setId(rs.getString(1));
+                a.setNom(rs.getString(2));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public void ajouterCoeff(Coeff d) {
+
+        try {
+
+            Statement st = c.createStatement();
+            String rq1 = "select * from coeff co inner join classe c on c.id=co.niveau_id "
+                    + "where c.niveau = (select niveau from classe where id= '" + d.getNiveau() + "') and co.matiere_id ='" + d.getMatiere() + "'";
+
+            String rq2 = "SELECT c.niveau FROM coeff co inner join classe c on c.id=co.niveau_id where co.niveau_id = '" + d.getNiveau() + "' and co.matiere_id='" + d.getMatiere() + "'  ";
+            String req = "insert into `coeff`" + "(`niveau_id`,`matiere_id`,`valeur`)" + " values('" + d.getNiveau() + "','" + d.getMatiere() + "','" + d.getValeur() + "')";
+
+            ResultSet myResultSet = st.executeQuery(rq1);
+            if (myResultSet.absolute(1)) {
+                System.out.println("errooooor");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Ajout d'un coefficient!");
+                alert.setHeaderText("Warning");
+                alert.setContentText(" Le coefficient de matière  est déjà affecté pour ce niveau ! ");
+                alert.showAndWait();
+            } else {
+                st.executeUpdate(req);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Ajout d'un coefficient !");
+                alert.setHeaderText("Information");
+                alert.setContentText("coefficient bien ajouté ");
+
+                alert.showAndWait();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ServiceClasse.class.getName()).log(Level.SEVERE, null, ex);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ajout d'un coefficient!");
+            alert.setHeaderText("Information");
+            alert.setContentText("coefficient non ajoutée!! ");
+            alert.showAndWait();
+        }
+
+    }
+
+    public List< Coeff> afficherCoeff() {
+
+        ArrayList< Coeff> myList = new ArrayList();
+        try {
+            PreparedStatement pt = c.prepareStatement("select c.id,c.valeur,cl.niveau,m.nom "
+                    + " from coeff c inner join matiere m on m.id=c.matiere_id "
+                    + " inner join classe cl on cl.id = c.niveau_id ");
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                Coeff a = new Coeff();
+                a.setId(rs.getInt(1));
+                a.setNiveau(rs.getString(3));
+                a.setMatiere(rs.getString(4));
+                a.setValeur(rs.getInt(2));
+
+                myList.add(a);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    }
+
+    public void modifierCoeff(Coeff co) {
+        String rq1 = "select * from coeff co inner join classe c on c.id=co.niveau_id "
+                + "where c.niveau = (select niveau from classe where id= '" + co.getNiveau() + "') and co.matiere_id ='" + co.getMatiere() + "'";
+
+        String requete = "UPDATE coeff SET niveau_id=? , matiere_id=? , valeur=?  WHERE id=?";
+        try {
+            Statement st = c.createStatement();
+            ResultSet myResultSet = st.executeQuery(rq1);
+            if (myResultSet.absolute(1)) {
+                System.out.println("errooooor");
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Ajout d'un coefficient!");
+                alert.setHeaderText("Warning");
+                alert.setContentText("Le coefficient de matière  est déjà affecté pour ce niveau! ");
+                alert.showAndWait();
+            } else {
+
+                PreparedStatement pst = c.prepareStatement(requete);
+
+                pst.setString(1, co.getNiveau());
+                pst.setString(2, co.getMatiere());
+                pst.setInt(3, co.getValeur());
+                pst.setInt(4, co.getId());
+                pst.executeUpdate();
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void supprimerCoeff(int id) {
+        String requete = "DELETE FROM coeff WHERE id =?";
+
+        try {
+            PreparedStatement pst = c.prepareStatement(requete);
+            pst.setInt(1, id);
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
--- src/Services/UserService.java
+++ src/Services/UserService.java
@@ -254,22 +254,22 @@ public class UserService {
         while (res.next()) {
             value = res.getString("password");
             String role=res.getString("roles");
-            
+
             if(role.contains("ENSEIGNANT"))
             {
-             System.setProperty("classeenseignant_id", res.getString("classeenseignant_id"));  
+             System.setProperty("classeenseignant_id", res.getString("classeenseignant_id"));
             }
             else if(role.contains("PARENT"))
             {
-               
+
             }
             else
             {
-             System.setProperty("classeeleve_id", res.getString("classeeleve_id"));   
+             System.setProperty("classeeleve_id", res.getString("classeeleve_id"));
             }
-            String id=Integer.toString(res.getInt("id"));   
+            String id=Integer.toString(res.getInt("id"));
             System.setProperty("id", id);
-            System.setProperty("role", role);         
+            System.setProperty("role", role);
             System.setProperty("email", res.getString("email"));
         }
         result = checkPassword(password, value);
@@ -280,4 +280,256 @@ public class UserService {
         return (int) (Math.random() * (9999 - 1000));
 
     }
+
+public ArrayList< User> afficherEns1 (){
+        
+        ArrayList< User> myList = new ArrayList();
+        
+        try {
+          
+                PreparedStatement pt =connexion.prepareStatement("select * from user u " 
+              + "inner join classe c on u.classeenseignant_id = c.id "); 
+                
+                String reqq="select * from `classe` where id = ? ";
+                 PreparedStatement cn =connexion.prepareStatement(reqq); 
+        
+            ResultSet rs= pt.executeQuery();
+            
+               
+            while(rs.next()){
+                 cn.setInt(1,rs.getInt(4));
+                 ResultSet cng=cn.executeQuery();
+            while(cng.next()){
+                User a = new User();
+                
+                a.setIdentifiant(rs.getInt(1));
+                a.setNom(rs.getString(16));//16
+                a.setPrenom(rs.getString(17));//17
+               // a.setLibC(cng.getString(4));
+                a.setClasseenseignant_id(cng.getString(4));
+                a.setEmail(rs.getString(7));
+                a.setDate_Embauche(rs.getString(18));
+                
+                System.out.println(cng.getString(4));
+               
+                myList.add(a);
+             
+                
+            }
+        }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    } 
+      
+   public List<User> rechercherEns1ParClasse (String classe){
+        
+        String requete="select u.id,u.nom,u.prenom,c.libelle,u.email,u.date_embauche from user u " 
+              + "inner join classe c on u.classeenseignant_id = c.id where c.libelle =?";
+      
+        ArrayList<User> myList = new ArrayList();
+        try {
+            
+             PreparedStatement pst = connexion.prepareStatement(requete);
+             pst.setString(1,classe);
+              
+            
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                User a = new User();
+                
+             a.setIdentifiant(rs.getInt(1));
+                a.setNom(rs.getString(2));//16
+                a.setPrenom(rs.getString(3));//17
+               a.setClasseenseignant_id(rs.getString(4));
+                a.setEmail(rs.getString(5));
+                a.setDate_Embauche(rs.getString(6));
+                System.out.println(rs.getString(4));
+                myList.add(a);
+        
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return myList;
+    }
+   
+   public ArrayList< User> afficherEleves (){
+        
+        ArrayList< User> myList = new ArrayList();
+         
+        try {
+          
+                PreparedStatement pt =connexion.prepareStatement("select u.id,u.nom,u.prenom,c.libelle,j.nom,j.prenom,j.email,u.date_inscription,u.classeeleve_id from user u " 
+                    + "inner join classe c on u.classeeleve_id = c.id inner join user j on u.parent_id = j.id ");    
+                    
+            ResultSet rs= pt.executeQuery();
+            while(rs.next()){
+                User a = new User();
+                
+                
+                a.setIdentifiant(rs.getInt(1));
+                a.setNom(rs.getString(2));//16
+                a.setPrenom(rs.getString(3));//17
+                //a.setLibC(rs.getString(4));
+                
+                a.setDate_Inscription(rs.getString(8));
+               a.setClasseeleve_id(rs.getString(4));
+               System.out.println(rs.getInt(9));
+                 System.out.println(rs.getString(5));
+                  System.out.println(rs.getString(6));
+               
+                myList.add(a);
+             
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    } 
+   
+   
+   public ArrayList< User> afficherEns (){
+        
+        ArrayList< User> myList = new ArrayList();
+         
+        try {
+          
+                PreparedStatement pt =connexion.prepareStatement("select * from user u " 
+              + "inner join classe c on u.classeenseignant_id = c.id ");    
+          
+            ResultSet rs= pt.executeQuery();
+            while(rs.next()){
+                User a = new User();
+                
+                a.setIdentifiant(rs.getInt(1));
+                a.setNom(rs.getString(16));//16
+                a.setPrenom(rs.getString(17));//17
+               // a.setClasse();
+               a.setClasseenseignant_id(rs.getString(4));
+              
+                myList.add(a);
+              
+                
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return myList;
+    } 
+   
+  public List< User>  ComboxGetIdEns(String nom){
+        ArrayList< User> myList = new ArrayList();
+         String requete="select id from `user` where nom = ? and roles = 'a:1:{i:0;s:15:\"ROLE_ENSEIGNANT\";}' ";
+         try {
+            PreparedStatement pst = connexion.prepareStatement(requete);
+           
+            pst.setString(1,nom);
+         
+            
+         ResultSet rs= pst.executeQuery(); 
+         
+         while(rs.next()){
+               
+              User a = new User();
+                a.setIdentifiant(rs.getInt(1));
+                
+                myList.add(a);
+                
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    return myList;
+    } 
+  
+  public List< User>  sEns(String ide){
+        ArrayList< User> myList = new ArrayList();
+         String requete="select email from `user` where id = ?";
+         try {
+            PreparedStatement pst = connexion.prepareStatement(requete);
+           
+            pst.setString(1,ide);
+         
+            
+         ResultSet rs= pst.executeQuery(); 
+         
+         while(rs.next()){
+               
+              User a = new User();
+                a.setEmail(rs.getString(1));
+                
+                myList.add(a);
+                
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    return myList;
+    } 
+   
+   public List< User>  ComboxGetMailEns(int id){
+        ArrayList< User> myList = new ArrayList();
+         String requete="select email from `user` where id = ?";
+         try {
+            PreparedStatement pst = connexion.prepareStatement(requete);
+           
+            pst.setInt(1,id);
+         
+            
+         ResultSet rs= pst.executeQuery(); 
+         
+         while(rs.next()){
+               
+              User a = new User();
+                //a.setId(rs.getInt(1));
+                a.setEmail(rs.getString(1));
+                
+                myList.add(a);
+                
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    return myList;
+    } 
+   
+   public List<User> rechercherEleveParClasse (String classe){
+        
+        String requete="select u.id,u.nom,u.prenom,c.libelle,j.nom,j.prenom,j.email,u.date_inscription,u.classeeleve_id  "
+                + " FROM user u  inner join classe c on c.id= u.classeeleve_id"
+                + " inner join user j on u.parent_id = j.id where c.libelle =?";
+      
+      
+        ArrayList<User> myList = new ArrayList();
+        try {
+            
+             PreparedStatement pst = connexion.prepareStatement(requete);
+             pst.setString(1,classe);
+              
+            
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                User a = new User();
+                
+              a.setIdentifiant(rs.getInt(1));
+                a.setNom(rs.getString(2));//16
+                a.setPrenom(rs.getString(3));//17
+               // a.setLibC(rs.getString(4));
+               
+                 a.setDate_Inscription(rs.getString(8));
+               a.setClasseeleve_id(rs.getString(4));
+                myList.add(a);
+        
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return myList;
+    }
+   
+
 }
